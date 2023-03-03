106fc13cdfe777288cdda1bc3de96441b035dc8d
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-03-03 11:03:03 +0530

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,7 +23,7 @@ const navItems = {
 function Logo() {
   return (
     <Link aria-label="Abhinav Tirumani" href="/">
-      <motion.svg fill="#ffffff" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1">
+      <motion.svg fill="#ffffff" width="70px" height="70px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1">
   <motion.path
     initial={{
       opacity: 0,
@@ -38,7 +38,7 @@ function Logo() {
       type: 'spring',
       stiffness: 50,
     }}
-    d="M18.97021,19.75732,15.34912,5.27246A2.9958,2.9958,0,0,0,12.43848,3h-.877A2.9958,2.9958,0,0,0,8.65088,5.27246L5.02979,19.75732a1.0001,1.0001,0,0,0,1.94042.48536L8.28082,15h7.43836l1.31061,5.24268a1.0001,1.0001,0,0,0,1.94042-.48536Z"
+    d="M18.97021,19.75732,15.34912,5.27246A2.9958,2.9958,0,0,0,12.43848,3h-.877A2.9958,2.9958,0,0,0,8.65088,5.27246L5.02979,19.75732a1.0001,1.0001,0,0,0,1.94042.48536L8.28082,15h7.43836l1.31061,5.24268a1.0001,1.0001,0,0,0,1.94042-.48536ZM8.78082,13l1.81049-7.24219A.99825.99825,0,0,1,11.56152,5h.877a.99825.99825,0,0,1,.97021.75781L15.21918,13Z"
     stroke="currentColor"
     strokeWidth={2}
     strokeLinecap="round"
