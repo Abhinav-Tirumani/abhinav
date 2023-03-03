6bb0e53bf9525f78573b274518b414829a5e2b68
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-03-03 08:23:44 +0530

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,45 +23,48 @@ const navItems = {
 function Logo() {
   return (
     <Link aria-label="Abhinav Tirumani" href="/">
-      <motion.svg
-        className="text-black dark:text-white h-[25px] md:h-[37px]"
-        width="25"
-        height="37"
-        viewBox="0 0 232 316"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          initial={{
-            opacity: 0,
-            pathLength: 0,
-          }}
-          animate={{
-            opacity: 1,
-            pathLength: 1,
-          }}
-          transition={{
-            duration: 0.5,
-            type: 'spring',
-            stiffness: 50,
-          }}
-          d="M39 316V0"
-          stroke="currentColor"
-          strokeWidth={78}
-        />
-        <motion.path
-          initial={{ x: -200, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            type: 'spring',
-            stiffness: 50,
-          }}
-          d="M232 314.998H129.852L232 232.887V314.998Z"
-          fill="currentColor"
-        />
-      </motion.svg>
-    </Link>
+  <motion.svg
+    className="text-black dark:text-white h-[25px] md:h-[37px]"
+    width="25"
+    height="37"
+    viewBox="0 0 232 316"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <motion.path
+      initial={{
+        opacity: 0,
+        pathLength: 0,
+      }}
+      animate={{
+        opacity: 1,
+        pathLength: 1,
+      }}
+      transition={{
+        duration: 0.5,
+        type: 'spring',
+        stiffness: 50,
+      }}
+      d="M39 316V0"
+      stroke="currentColor"
+      strokeWidth={78}
+    />
+    <motion.path
+      initial={{ x: -200, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        duration: 0.5,
+        type: 'spring',
+        stiffness: 50,
+      }}
+      d="M232 314.998H129.852L232 232.887V314.998Z"
+      fill="currentColor"
+      // change the L to A in the d attribute
+      d="M232 314.998H129.852L203.83 152.644L176.03 152.644L116 314.998H25.369L105.754 126.325L127.985 126.325L232 314.998Z"
+    />
+  </motion.svg>
+</Link>
+
   );
 }
 
